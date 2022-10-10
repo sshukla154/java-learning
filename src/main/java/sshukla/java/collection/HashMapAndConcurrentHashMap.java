a4ddf03782f7b8bf966c83d6d439cdae3bbeb227
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2022-10-10 11:45:00 +0200

    Refactoring

--- src/main/java/sshukla/java/collection/HashMapAndConcurrentHashMap.java
+++ src/main/java/sshukla/java/collection/HashMapAndConcurrentHashMap.java
@@ -9,33 +9,35 @@ import java.util.concurrent.ConcurrentHashMap;
 public class HashMapAndConcurrentHashMap extends Thread {
 
     /**
-     * Will encounter `java.util.ConcurrentModificationException` when we use HashMap because from main thread we are reading the values and in between from child thread we started modifying the values
+     * Will encounter `java.util.ConcurrentModificationException` when we use HashMap because from main thread we are
+     * reading the values and in between from child thread we started modifying the values - It has lock on entire map object
      * <p>
-     * No error when we use ConcurrentHashMap()
+     * No error when we use ConcurrentHashMap() - It has segment locking or bucket locking
      **/
 //    static Map<Integer, String> hashMap = new HashMap<>();
-    static Map<Integer, String> hashMap = new ConcurrentHashMap<>();
+    static Map<Integer, String> map = new ConcurrentHashMap<>();
 
     public void run() {
         try {
             Thread.sleep(1000);
             System.out.println("I am writing from child thread after 1000MS ===> ");
-            hashMap.put(4, "D");
+            map.put(4, "D");
         } catch (InterruptedException exception) {
             System.out.println("I encountered InterruptedException !!!!");
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        hashMap.put(1, "A");
-        hashMap.put(2, "B");
-        hashMap.put(3, "C");
+        map.put(1, "A");
+        map.put(2, "B");
+        map.put(3, "C");
         HashMapAndConcurrentHashMap hashMapAndConcurrentHashMap = new HashMapAndConcurrentHashMap();
         hashMapAndConcurrentHashMap.start();
-        for (Integer i : hashMap.keySet()) {
+        for (Integer i : map.keySet()) {
             System.out.println("I am reading from main thread ===> " + i);
             Thread.sleep(10000);
         }
+        System.out.println(map);
 
     }
 
